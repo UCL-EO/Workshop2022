dd5155843e157a44761b755ca13db5adf7015f0b
Feng Yin <marcyin@sci4.jasmin.ac.uk>
2022-03-20 19:03:47 +0000

    add local cache data

---
+++ notebooks/data_downloader.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import numpy as np
+from multiprocessing import Pool
+def downloader(url):
+    fname = 'data/' + url.split('/')[-1]
+    if not os.path.exists(fname):
+        r = requests.get(url)
+        with open(fname, 'wb') as f:
+            f.write(r.content)
+        
+bio_urls = np.loadtxt('data/bio_urls.txt', dtype='str')
+p = Pool(4)
+p.map(downloader, bio_urls)
+p.close()
+p.join()
+
+ens_urls = np.loadtxt('data/ens_urls.txt', dtype='str')
+p = Pool(4)
+p.map(downloader, ens_urls)
+p.close()
+p.join()
